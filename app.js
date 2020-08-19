7b3a42b436146808b8e1eddede6a017c23d3ccae
ramsses arenas <ramsses9520@gmail.com>
2020-08-19 17:34:00 -0500

    JS Funciones 1

--- app.js
+++ app.js
@@ -1,41 +1,26 @@
-//Crear objeto
-/*
-const persona = {
-     nombre: 'miguel',
-     apellido : 'martinez',
-     profesion : 'diseñador grafico',
-     edad: 20,
-     email : 'mail@correo.com',
-     musica : ['punk','rock','Heavy metal'],
-     hogar:{
-          ciudad: 'guadalajara',
-          pais: 'mexico'
-     },
-     nacimiento: function(){
-          return new Date().getFullYear()-this.edad;
-          //return new Date().getFullYear()-edad;
-     }
+// Function Declaretion
+
+/* function saludar(nombre){
+     console.log(`Hola ${nombre}`);
 }
 
-persona.musica.push('alternativo');
-console.log (persona.musica[1]);
-console.log (persona.hogar.ciudad);
-console.log (persona.nacimiento());
+saludar('alma');
+saludar('miguel');
+saludar('rogelio'); */
 
-*/
+function sumar(a,b){
+     console.log(a+b);
+}
+sumar(1,2);
+sumar(4,2);
 
 
-// Arreglo objetos
-const autos = [
-     {modelo: 'mustang',motor: 6.0 },
-     {modelo: 'camaro',motor: 6.1 },
-     {modelo: 'challenger',motor: 6.3 }
-];
-
-for(let i=0; i<autos.length; i++){
-     //console.log(autos[i].modelo);
-     console.log(`${autos[i].modelo} ${autos[i].motor}`);
+function saludar(nombre = 'Vistante'){
+     //if(typeof nombre === 'undefined'){nombre='Visitante'}
+     //Manera antigua
+     return `Hola ${nombre}`;
 }
 
-autos[0].modelo = 'audi';
-console.log(autos);
+let saludo;
+ saludo = saludar();
+  console.log(saludo);
