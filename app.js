fc782497a0eeb0b8de84e87246fdd335b952cf74
ramsses arenas <ramsses9520@gmail.com>
2020-08-19 13:40:21 -0500

    JS Template Literals o String Template

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-let cp;
-cp = 42119;
-cp = String(cp);
+const     producto1 =  'Pizza',
+          precio1 = 30,
+          producto2 = 'Hamburguesa',
+          precio2 = 40;
 
-//console.log(cp.length);
+let html;
+/*
+html =    '<ul>' + 
+          '<li>Orden: ' + producto1 + '</li>'+
+          '<li>Precio:' + precio1 + '</li>'+
+          '<li>Orden: ' + producto2 + '</li>'+
+          '<li>Precio: '+ precio2 + '</li>' +
+          '<li>Total: '+ (precio1+precio2) + '</li>' +
+          '</ul>';
+          */
 
-let dato;
-dato = '4'+'4;'
-dato = true;
+html =    `
+          <ul>
+               <li>Orden: ${producto1}</li>
+               <li>Precio: ${precio1}</li>
+               <li>Orden: ${producto2}</li>
+               <li>Precio: ${precio2}</li>
+               <li>Total: ${total(precio1,precio2)}</li>
+          </ul>
+          `
+function total(precio1,precio2){
+     return precio1+precio2;
+}
 
-// De arreglo a string
-
-dato =  String([1,2,3])
-
-//to String
-dato = 20;
-dato = true;
-dato = [1,2,3,4];
-//dato = null;
-//dato = undefined;
-dato = dato.toString();
-
-console.log(dato);
-console.log(dato.length);
-console.log(typeof dato);
-
-
-
+document.getElementById('app').innerHTML = html;
