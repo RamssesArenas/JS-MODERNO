1c330d44e4809faca229685d9ca616f1f2fa97c0
ramsses arenas <ramsses9520@gmail.com>
2020-08-19 01:01:41 -0500

    JS string a numeros 2

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ const numero3 = 'tres';
 
 
 
-console.log(Number(numero1)+numero2);
-console.log(parseInt(numero1)+numero2);
-console.log(typeof parseInt(numero2));
-console.log(typeof numero3);
-console.log(parseInt(numero3));
+let dato;
+dato = Number("20");
+dato = Number("20.22214");
+dato = Number(true);
+dato = Number(false);
+dato = Number(null);
+dato = Number(undefined);
+dato = Number('hola mundo');
+dato = Number([1,2,3,4]);
+
+dato = parseInt('100');
+dato = parseFloat('100');
+dato = parseInt('100.2453');
+
+dato = '113434.31412';
+
+//console.log(dato.toFixed(3)); to fix acortar decimales
+console.log(parseFloat(dato).toFixed(2));
+
+console.log(dato);
 
 
 
