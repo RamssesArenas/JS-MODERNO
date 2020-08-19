d98cf814a990b449c98241e02d81363c23e48d5c
ramsses arenas <ramsses9520@gmail.com>
2020-08-19 00:35:37 -0500

    JS convertir strings a numeros

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-const numero1 = 20;
-const numero2 = 50;
-const numero3 = '20';
-const nombre = 'ramsses';
-const nombre2 = 'josue';
+const numero1 = '50';
+const numero2 = 10;
+const numero3 = 'tres';
 
-console.log(numero2>numero1);
-console.log(numero1>numero2);
-console.log(20=='20');
-console.log(20==='20');//comparador que revisa el tipo de dato
-console.log(numero1===numero3);
-console.log(2 != 3);
-console.log('hola'== ' hola');
-console.log('z'>'b'); //ABCDF menor rango y abcdf mayor rango
-console.log(null == undefined);
-console.log(null === undefined);
-console.log(nombre == nombre2);
+
+
+console.log(Number(numero1)+numero2);
+console.log(parseInt(numero1)+numero2);
+console.log(typeof parseInt(numero2));
+console.log(typeof numero3);
+console.log(parseInt(numero3));
+
+
+
+
