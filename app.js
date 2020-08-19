27778ec0430280fa21c8cef50dfe3fa112b97665
ramsses arenas <ramsses9520@gmail.com>
2020-08-18 23:55:16 -0500

    JS Tipos de Dato

--- app.js
+++ app.js
@@ -1,37 +1,18 @@
-// Numeros
-
-const numero1 = 30;
-const numero2 = 20;
-const numero3 = 20.20;
-const numero4 = .1020;
-const numero5 = -3;
-
-let resultado;
-
-resultado = numero1+numero2;
-resultado = numero1-numero2;
-resultado = numero1*numero2;
-resultado = numero1/numero2;
-resultado = numero1*2;
-resultado = numero1%numero2;
-resultado = Math.PI;
-resultado = Math.round(2.4);
-resultado = Math.floor(2.4);
-resultado = Math.sqrt(144);
-resultado = Math.abs(numero5);
-resultado = Math.pow(8,3);
-resultado = Math.min(6,7,3,8,4);
-resultado = Math.max(6,7,3,8,4);
-resultado = Math.random();
-resultado = 10+20-5*2;
-
-let puntaje = 10;
+let valor;
+valor  = 'ramsses';
+valor = 25;
 
 
-
-puntaje ++;
-
-console.log('tu puntaje es ' + puntaje);
-
-
-console.log(resultado);
+// bollean
+valor=true;
+//null
+valor=null;
+//symbol
+valor = Symbol('simbolo');
+//arreglo
+valor = [1,2,3,4]
+//objeto
+valor = { nombre: 'juan'}
+//Fecha
+valor = new Date();
+console.log(typeof valor);
