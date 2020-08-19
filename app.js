e421326c8727bbde5dd59e119bdd59761a250646
ramsses arenas <ramsses9520@gmail.com>
2020-08-18 23:07:47 -0500

    variables js

---
+++ app.js
@@ -0,0 +1,49 @@
+/*let nombre = prompt("cual es tu nombre?");
+let edad = prompt("cual es tu edad?");
+
+document.getElementById('app').innerHTML=`Bienvenido ${nombre} de ${edad} años`;
+
+console.log(2+2);
+
+var nombre = 'juan';
+var nombre = 'jhon'
+
+console.log(nombre);
+
+var let const
+
+
+*/
+
+let nombre,mensaje;
+
+mensaje = "Y entonces dije \'buen curso\' estoy";
+
+//console.log(mensaje);
+
+mensaje = 'aprendiendo javaScript, CSS, HTML para ser Frontend'
+
+let aprendiendo = 'aprrendiendo',
+     tecnologia = 'javasrcript' +' ';
+
+
+     //concatenar js
+     /*console.log(aprendiendo+' '+tecnologia);
+     console.log(`${aprendiendo}${tecnologia}`);
+
+     console.log(tecnologia.length);
+
+     console.log(tecnologia.concat(' ', 'es genial'));
+
+     console.log(tecnologia.toUpperCase);
+
+     console.log(mensaje.indexOf('PHP'));
+
+     console.log(mensaje.slice(-3));*/
+
+     console.log(mensaje.replace('CSS','PHP'));
+
+     console.log(mensaje.includes('CSS','PHP'));
+
+
+     console.log(tecnologia.repeat(10));
