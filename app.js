2dea9a09afb36846a0ca1cc3d28ded2ed4e5de89
ramsses arenas <ramsses9520@gmail.com>
2020-08-19 00:08:00 -0500

    JS Operadores de Comparacion

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-let valor;
-valor  = 'ramsses';
-valor = 25;
+const numero1 = 20;
+const numero2 = 50;
+const numero3 = '20';
+const nombre = 'ramsses';
+const nombre2 = 'josue';
 
-
-// bollean
-valor=true;
-//null
-valor=null;
-//symbol
-valor = Symbol('simbolo');
-//arreglo
-valor = [1,2,3,4]
-//objeto
-valor = { nombre: 'juan'}
-//Fecha
-valor = new Date();
-console.log(typeof valor);
+console.log(numero2>numero1);
+console.log(numero1>numero2);
+console.log(20=='20');
+console.log(20==='20');//comparador que revisa el tipo de dato
+console.log(numero1===numero3);
+console.log(2 != 3);
+console.log('hola'== ' hola');
+console.log('z'>'b'); //ABCDF menor rango y abcdf mayor rango
+console.log(null == undefined);
+console.log(null === undefined);
+console.log(nombre == nombre2);
