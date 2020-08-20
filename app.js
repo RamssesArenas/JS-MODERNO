e1ff0e3b8b86370617ce8fb20b24c9882ee0e21b
RamssesArenas <l15200119@pachuca.tecnm.mx>
2020-08-19 20:46:57 -0500

    Function Expression

--- app.js
+++ app.js
@@ -1,30 +1,15 @@
-// Function Declaretion
+// Function Expression
 
-/* function saludar(nombre){
-     console.log(`Hola ${nombre}`);
+const suma = function(a,b){
+     return a+b;
 }
 
-saludar('alma');
-saludar('miguel');
-saludar('rogelio'); 
+console.log(suma(1,2));
+console.log(suma(4,2));
 
-
-*/
-
-function sumar(a,b){
-     console.log(a+b);
-}
-sumar(1,2);
-sumar(4,2);
-
-
-function saludar(nombre = 'Vistante'){
-     //if(typeof nombre === 'undefined'){nombre='Visitante'}
-     //Manera antigua
-     return `Hola ${nombre}`;
+const saludar = function(nombre = 'visitante', edad=20, trabajo='desarrollador web'){
+     return `hola ${nombre}, profesion ${trabajo} y tienes ${edad} años`;
 }
 
-let saludo;
- saludo = saludar();
-  console.log(saludo);
-
+console.log(saludar('ram'));
+console.log(saludar());
