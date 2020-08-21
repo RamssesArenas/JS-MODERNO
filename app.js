ac85ebd33ad8024efc7bf5a2dd242f0c7e226b12
RamssesArenas <l15200119@pachuca.tecnm.mx>
2020-08-20 22:55:37 -0500

    Iteraciones - For Loop

--- app.js
+++ app.js
@@ -1,46 +1,21 @@
-// switch
-/* const metodoPago='efectivo';
+// for loops
+/* for (let i = 0; i < 10; i++) {
+     console.log(`Numero: ${i+1}`);
+     
+} 
 
-switch (metodoPago) {
-     case 'efectivo':console.log(`El usuario pago con ${metodoPago}`)
-          break;
-     case 'cheque':console.log(`El usuario pago con ${metodoPago}`)
-          break;
-     case 'tarjeta':console.log(`El usuario pago con ${metodoPago}`)
-          break;
-     default:
-          console.log('metodo de pago no soportado');
+ for (let i = 0; i < 10; i++) {
+     if (i === 5) {
+          console.log('voy en el 5');
+          break;// Continue seguir y brack detener el for
+     }
+     console.log(`${i+1}`);
 } */
 
-let mes;
-switch (new Date().getMonth()) {
-     case 0:mes = 'Enero';
-          break;
-     case 1:mes = 'Febrero';
-          break;
-     case 2:mes = 'Marzo';
-          break;
-     case 3:mes = 'Abril ';
-          break;
-     case 4:mes = 'Mayo';
-          break;
-     case 5:mes = 'Junio';
-          break;
-     case 6:mes = 'Julio';
-          break;
-     case 7:mes = 'Agosto';
-          break;
-     case 8:mes = 'Septiembre';
-          break;
-     case 9:mes = 'Octubre';
-          break;
-     case 10:mes = 'Noviembre';
-          break;
-     case 11:mes = 'Diciembre';
-          break;
-     default:
-          console.log('Este mes no existe');
+const arreglosProductos = ['camisa','boleto','guitarra','disco'];
+
+
+
+for (let i = 0; i < arreglosProductos.length; i++) {
+          console.log(`tu producto ${arreglosProductos[i]} fue agregado`);
 }
-
-console.log(`Este mes es ${mes}`);
-
